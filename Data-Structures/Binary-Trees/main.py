6d86d32e98726ff3d352f503764b7ce2020bd1a3
danofoundation <dan.dev947366@gmail.com>
2024-07-09 00:03:47 +0300

    Update BinaryTree

--- Data-Structures/Binary-Trees/main.py
+++ Data-Structures/Binary-Trees/main.py
@@ -27,13 +27,32 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
             
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:  
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
             
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
 
-print(my_tree.root)
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+print(my_tree.contains(27))
+print(my_tree.contains(17))
+
+# print(my_tree.root)
+# print(my_tree.root.value)
+# print(my_tree.root.left.value)
+# print(my_tree.root.right.value)
