65d5b79a23542eab1dad8c8a9219a807b8cfba01
danofoundation <dan.dev947366@gmail.com>
2024-07-11 08:13:35 +0300

    Update heap

--- Data-Structures/Heaps/main.py
+++ Data-Structures/Heaps/main.py
@@ -34,3 +34,17 @@ class MaxHeap:
         self._sink_down(0)
         
         return max_value
+        
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+            if self.heap[left_index] > self.heap[max_index]:
+                max_index = left_index
+                
+            if self.heap[right_index] > self.heap[max_index]:
+                max_index = right_index
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
