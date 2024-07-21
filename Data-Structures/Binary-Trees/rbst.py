9519a6e288baf249142415b40281d2225bbd2bca
danofoundation <dan.dev947366@gmail.com>
2024-07-21 18:46:12 +0300

    Update rbst

--- Data-Structures/Binary-Trees/rbst.py
+++ Data-Structures/Binary-Trees/rbst.py
@@ -69,6 +69,33 @@ class BinarySearchTree:
             self.root = Node(value)
         self.__r_insert(self.root, value)  
 
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+        
+    def __delete_node(self, current_node, value):
+	    if current_node == None: 
+		    return None
+	    if value < current_node.value:
+		    current_node.left = self.__delete_node(current_node.left, value)
+	    elif value > current_node.value: 
+		    current_node.right = self.__delete_node(current_node.right, value)
+	    else:
+		    if current_node.left == None and current_node.right == None:
+			    return None
+		    elif current_node.left == None:
+			    current_node = current_node.right
+		    elif current_node.right == None:
+			    current_node = current_node.left
+		    else:
+			    sub_tree_min = self.min_value(current_node.right)
+			    current_node.value = sub_tree_min
+			    current_node.right = self.__delete_node(current_node.right, sub_tree_min)
+	    return current_node
+    
+    def delete_node(self, value):
+        self.root = self.__delete_node(self.root, value)
 
 
 
@@ -78,27 +105,42 @@ my_tree.r_insert(1)
 my_tree.r_insert(3)
 
 """
-    THE LINES ABOVE CREATE THIS TREE:
-                 2
-                / \
-               1   3
+       2
+      / \
+     1   3
+"""
+
+print("root:", my_tree.root.value)
+print("root.left =", my_tree.root.left.value)
+print("root.right =", my_tree.root.right.value)
+
+
+my_tree.delete_node(2)
+
+"""
+       3
+      / \
+     1   None
 """
 
 
-print('Root:', my_tree.root.value)            
-print('Root -> Left:', my_tree.root.left.value)        
-print('Root -> Right:', my_tree.root.right.value)    
+print("\nroot:", my_tree.root.value)
+print("root.left =", my_tree.root.left.value)
+print("root.right =", my_tree.root.right)
 
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-	Root: 2
-	Root -> Left: 1
-	Root -> Right: 3
+	root: 2
+	root.left = 1
+	root.right = 3
+
+	root: 3
+	root.left = 1
+	root.right = None
 
 """
-
 
 
