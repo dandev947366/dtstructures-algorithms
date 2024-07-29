0035164dec3225c4c43c4f8eeda2273e0ba268f0
danofoundation <dan.dev947366@gmail.com>
2024-07-29 20:52:02 +0300

    Update quicksort

--- Algorithms/Sorting/Quick-Sort/main.py
+++ Algorithms/Sorting/Quick-Sort/main.py
@@ -14,34 +14,31 @@ def pivot(my_list, pivot_index, end_index):
     swap(my_list, pivot_index, swap_index)
     return swap_index
 
+def quick_sort_helper(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index-1)  
+        quick_sort_helper(my_list, pivot_index+1, right)       
+    return my_list
+    
 
+def quick_sort(my_list):
+    quick_sort_helper(my_list, 0, len(my_list)-1)
+
+ 
+ 
 
 
 my_list = [4,6,1,7,3,2,5]
 
-print('List before running pivot():')
+quick_sort(my_list)
+
 print(my_list)
-
-returned_pivot_index = pivot(my_list, 0, 6)
-
-print('\nList after running pivot():')
-print(my_list)
-
-print('\nPivot Index:')
-print(returned_pivot_index)
 
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    List before running pivot():
-    [4, 6, 1, 7, 3, 2, 5]
-
-    List after running pivot():
-    [2, 1, 3, 4, 6, 7, 5]
-
-    Pivot Index:
-    3
-
+    [1, 2, 3, 4, 5, 6, 7]
  """
